613ca1159f13f94a87bc1d59f94d547ab55a82f3
irfanbf <irfanramadhan2626@gmail.com>
2019-08-10 13:38:43 +0700

    versi 2.22.0

---
+++ resources/views/produk.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>VapeStore</title>
+	</head>
+	<body><br>
+	<h2 align="center">Vape Shop<u></u></h2><br><br><br>
+	<p align="center">
+	<img src="{{ url('/img/orion.jpg') }}" 
+	<img src="{{asset('resources/views/tesla.jpg')}}">
+
+	</body>
+</html>
---
+++ routes/image.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Latihan image link</title>
+	</head>
+	<body><br>
+	<h2 align="center"><u>HERO DOTA 2</u></h2><br><br><br>
+	<h3 align="center">Klik Gambar Untuk Mengetahui Deskripsi</h3>
+	<p align="center">
+	<a  href="deskripsi dota2.html"> <img width="400" height="299" src ="Phantom-Assassin-Arcana.png"></a>
+	<a  href="deskripsi dota2-2.html"> <img width="400" height="299" src ="36-Gambar-Anti-Mage-Dota-2-Wallpaper-30.jpg"></a><br>
+	<a  href="deskripsi dota2-3 razor.html"> <img width="400" height="299" src ="508498474_preview_loadingscreen.jpg"></a>
+	<a  href="deskripsi dota2-3 pudge.html"> <img width="400" height="299" src ="mad.jpg"></a></p>
+
+	
+	<audio autoplay>
+
+	<source src="Dota.mp3" type="audio/ogg">
+	</audio>
+	</body>
+</html>
